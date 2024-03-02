e17af6e4cde5d0da99a676a5cb383dd9eb75f066
NIL2000 <nilabhra09b.net@gmail.com>
2024-03-02 12:03:58 +0530

    ✔ Completed Auth part, Added CORS in BE

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -8,43 +8,68 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
-  function handleSubmit() {
-    // e.preventDefault();
-    console.log("button is clicked");
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:3000/auth/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.token);
+        navigate("/home");
+      });
+  };
   return (
     <Card className="w-[350px] shadow-2xl border-2 border-cyan-900">
-      <CardHeader className="items-center">
-        <CardTitle className="text-3xl arvo-bold">Login</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form>
+      <form>
+        <CardHeader className="items-center">
+          <CardTitle className="text-3xl arvo-bold">Login</CardTitle>
+        </CardHeader>
+        <CardContent>
           <div className="grid w-full items-center gap-4 poppins-regular">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name" className="text-lg">
-                Username
+              <Label htmlFor="email" className="text-lg">
+                Email
               </Label>
-              <Input id="name" placeholder="username" />
+              <Input
+                id="email"
+                placeholder="username"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name" className="text-lg">
+              <Label htmlFor="password" className="text-lg">
                 Password
               </Label>
-              <Input id="name" placeholder="password" type="password" />
+              <Input
+                id="password"
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-center">
-        <Button
-          className="bg-slate-500 hover:bg-slate-800 hover:text-white hover:border-0 text-md"
-          onClick={() => handleSubmit}
-        >
-          SIGN IN
-        </Button>
-      </CardFooter>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button
+            className="bg-slate-500 hover:bg-slate-800 hover:text-white hover:border-0 text-md"
+            onClick={handleSubmit}
+          >
+            SIGN IN
+          </Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
 import {
   Card,
   CardContent,
@@ -9,41 +9,96 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const handleSubmit: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    console.log(`{email:${email}} {name:${name}}`);
+    await axios
+      .post("http://localhost:3000/auth/register", {
+        name,
+        email,
+        password,
+      })
+      .then(async (res) => {
+        console.log(res);
+        await axios
+          .post("http://localhost:3000/auth/login", {
+            email,
+            password,
+          })
+          .then((res) => {
+            console.log(res);
+            localStorage.setItem("token", res.data.token);
+            navigate("/home");
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Card className="w-[350px] shadow-2xl border-2 border-cyan-900">
-      <CardHeader className="items-center">
-        <CardTitle className="text-3xl arvo-bold">Register</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form>
+      <form>
+        <CardHeader className="items-center">
+          <CardTitle className="text-3xl arvo-bold">Register</CardTitle>
+        </CardHeader>
+        <CardContent>
           <div className="grid w-full items-center gap-4 poppins-regular">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name" className="text-lg">
-                Username
+                Name
               </Label>
-              <Input id="name" placeholder="email" />
+              <Input
+                id="name"
+                placeholder="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name" className="text-lg">
+              <Label htmlFor="email" className="text-lg">
                 Email
               </Label>
-              <Input id="name" placeholder="email" />
+              <Input
+                id="email"
+                placeholder="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name" className="text-lg">
+              <Label htmlFor="password" className="text-lg">
                 Set Password
               </Label>
-              <Input id="name" placeholder="password" type="password" />
+              <Input
+                id="password"
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-center">
-        <Button className="bg-slate-500 hover:bg-slate-800 hover:text-white hover:border-0 text-md">
-          SIGN UP
-        </Button>
-      </CardFooter>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button
+            className="bg-slate-500 hover:bg-slate-800 hover:text-white hover:border-0 text-md"
+            onClick={handleSubmit}
+            type="submit"
+          >
+            SIGN UP
+          </Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
