73a97793adb5a2acd1b0a7018c6c2f7717f73285
NIL2000 <nilabhra09b.net@gmail.com>
2024-04-08 23:52:12 +0530

    ✔ Partial stream page + socket introduced client side

--- frontend/src/pages/Streampage.tsx
+++ frontend/src/pages/Streampage.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from '../components/Navbar'
-import { Mic, Podcast, Video, Youtube } from 'lucide-react'
+import { Mic, MicOff, Podcast, Video, VideoOff, Youtube } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-
+import io from "socket.io-client";
+const Socket = io();
 export default function Streampage() {
+  const [isMicOn, setIsMicOn] = useState(true);
+  const [isVideoOn, setIsVideoOn] = useState(true);
+  const [isStreaming, setIsStreaming] = useState(false);
+  const [socket, setSocket] = useState<typeof Socket>();
+  const [mediaState, setMediaState] = useState({ audio: false, video: false });
+  let mediaObj = { media: null };
+  const [mediaStream, setMediaStream] = useState<MediaStream>();
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [ytString, setYtString] = useState<string>('');
+  useEffect(() => {
+    const newSocket = io("http://localhost:3002", {
+      transports: ["websocket", "polling"],
+    });
+    return () => {
+      newSocket.disconnect();
+    }
+  }, [])
+
+  useEffect(() => {
+    setMediaState({ audio: true, video: isVideoOn });
+    const initializeMediaStream = async () => {
+      try {
+        const media = await navigator.mediaDevices.getUserMedia(mediaState);
+        setMediaStream(media);
+        videoRef.current!.srcObject = media;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    initializeMediaStream();
+    return () => {
+      mediaStream?.getTracks().forEach((track) => track.stop());
+    }
+  }, [isVideoOn]);
+
+  const toggleAudio = () => {
+    // console.log(!isMicOn)
+    // setIsMicOn(!isMicOn);
+  }
+
+  const toggleVideo = () => {
+    console.log(!isVideoOn);
+    setIsVideoOn(!isVideoOn);
+  }
+
+  const startStreaming = () => {
+    // setIsStreaming(true);
+    // const mediaRecorder = new MediaRecorder(mediaObj.media, {
+    //   audioBitsPerSecond: 128000,
+    //   videoBitsPerSecond: 2500000,
+    //   framerate: 30,
+    // });
+  }
+
+
   return (
     <div className='flex justify-center'>
       <Navbar />
@@ -13,27 +69,28 @@ export default function Streampage() {
         </h3>
         <div className='w-full flex justify-center gap-4 mt-5'>
           <div className='h-[300px] w-full bg-slate-500 rounded-lg relative'>
-            <div>
-
-            </div>
+            <video className='w-full h-full z-2' autoPlay muted ref={videoRef}></video>
             <div className='flex gap-3 z-1 absolute bottom-4 w-full justify-center'>
-              <Button>
-                <Mic />
+              <Button onClick={toggleAudio}>
+                {!isMicOn && <Mic />}
+                {isMicOn && <MicOff />}
               </Button>
-              <Button>
-                <Video />
+              <Button onClick={toggleVideo}>
+                {!isVideoOn && <Video />}
+                {isVideoOn && <VideoOff />}
               </Button>
             </div>
           </div>
           <div className='w-full h-[300px] bg-stone-800 p-2'>
             <h3 className='text-white poppins-regular text-2xl'>Connections possible</h3>
-            <div className='flex'>
+            <div className='flex items-center gap-2 px-4'>
               <Youtube className='text-red-600 h-14 w-14' />
+              <input type="text" value={ytString} onChange={(e) => setYtString(e.target.value)} className='pt-1 p-2 w-full rounded-md' />
             </div>
           </div>
         </div>
         <div className='w-full flex justify-center mt-6'>
-          <Button className='bg-my-chat poppins-regular hover:bg-my-chat hover:bg-opacity-90 text-lg'>Start streaming</Button>
+          <Button className={`${!isStreaming ? `bg-green-500 hover:bg-green-400` : `bg-red-500 hover:bg-red-400`} poppins-regular text-lg`} onClick={startStreaming}>{isStreaming ? `Stop streaming` : `Start streaming`}</Button>
         </div>
       </div>
     </div>
